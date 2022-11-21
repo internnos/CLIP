330d2d007f9d19fb2319ce8af96b42d221cb69e1
internos <amajidsinar@gmail.com>
2022-11-21 17:55:31 +0700

    remove text embedding

--- clip/model.py
+++ clip/model.py
@@ -355,21 +355,11 @@ class CLIP(nn.Module):
 
         return x
 
-    def forward(self, image, text):
+    def forward(self, image):
         image_features = self.encode_image(image)
-        text_features = self.encode_text(text)
-
-        # normalized features
-        image_features = image_features / image_features.norm(dim=1, keepdim=True)
-        text_features = text_features / text_features.norm(dim=1, keepdim=True)
-
-        # cosine similarity as logits
-        logit_scale = self.logit_scale.exp()
-        logits_per_image = logit_scale * image_features @ text_features.t()
-        logits_per_text = logits_per_image.t()
 
         # shape = [global_batch_size, global_batch_size]
-        return logits_per_image, logits_per_text
+        return image_features
 
 
 def convert_weights(model: nn.Module):
