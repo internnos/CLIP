49550216faa6aa9e05289b44378461e5bfe7ef71
internos <amajidsinar@gmail.com>
2022-12-15 18:39:50 +0700

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -78,7 +78,7 @@ def _convert_image_to_rgb(image):
 
 def _transform(n_px):
     return Compose([
-        Resize(n_px, interpolation=BICUBIC),
+        Resize((n_px, n_px), interpolation=BICUBIC),
         _convert_image_to_rgb,
         ToTensor(),
         Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
